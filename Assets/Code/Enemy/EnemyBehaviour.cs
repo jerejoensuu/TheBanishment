aa62053fd5ecd0f10293896b2a8c5f4631b773ea
Joni <joni.nevala@gmail.com>
2022-10-12 04:43:07 +0300

    Enemy sight changes

--- Assets/Code/Enemy/EnemyBehaviour.cs
+++ Assets/Code/Enemy/EnemyBehaviour.cs
@@ -5,44 +5,102 @@ using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    public Vector3 targetDestination;
-    public float restingTime;
-    public float playerDetectionRange;
-    public float movementSpeed;
-    public float chaseSpeed;
-    private NavMeshAgent agent;
+    private int state; // 0 idle, 1 alert, 2 chasing, 3 attacking
+
+
+    [Header("Movement")]
+    [Tooltip("Navigating towards this position")] public Vector3 targetDestination;
+    [Tooltip("Time spent idle after reaching a destination")] public float waitTime;
+    public float idleMoveSpeed;
+    public float chaseMoveSpeed;
+
+    [Header("Detection")]
+    [Tooltip("The width of the sight cone. 0 = 180 degrees, 0.5 = 90 degrees")] public float detectionArea;
+    public float idleSightRange;
+    public float alertSightRange;
+    public float chaseSightRange;
+
+    [Header("Objects")]
     public Transform player;
     public Transform[] pointsOfInterest;
-    private bool idle = true;
-    private bool resting = false;
+    private NavMeshAgent agent;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        agent.speed = movementSpeed;
+        agent.speed = idleMoveSpeed;
     }
 
     private void Update()
     {
-        targetDestination = ChangeDestination();
+        if (SightCheck())
+        {
+            SetState(2);
+        }
+        else
+        {
+            SetState(0);
+        }
+        //else if (SoundCheck())
+        //{
+        //    SetState(1);
+        //}
+
+        //targetDestination = ChangeDestination();
     }
 
+    private void SetState(int value)
+    {
+        state = value;
+    }
+
+    private bool SightCheck()
+    {
+        bool playerDetected = false;
+
+        Vector3 dir = Vector3.Normalize(player.position - transform.position);
+        float dot = Vector3.Dot(dir, transform.forward);
+
+        if (dot > detectionArea) // Player is inside detection area (front of enemy)
+        {
+            float rayLength = idleSightRange;
+            if (state == 1) { rayLength = alertSightRange; }
+            else if (state == 2) { rayLength = chaseSightRange; }
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, (player.position - transform.position), out hit, rayLength)) // Raycast towards player to see if anything's blocking vision
+            {
+                if (hit.transform.gameObject.tag == "Player") // Ray hits player
+                {
+                    playerDetected = true;
+                    Debug.Log(playerDetected);
+                    Debug.DrawRay(transform.position, (player.position - transform.position) * hit.distance, Color.yellow);
+                }
+            }
+
+        }
+        return playerDetected;
+    }
+
+    /*
     private Vector3 ChangeDestination()
     {
+
+
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
-        if (distanceToPlayer < playerDetectionRange) // Paths to player if nearby
+        if (distanceToPlayer < idleSightRange) // Paths to player if nearby
         {
             idle = false;
             resting = false;
-            agent.speed = chaseSpeed;
+            agent.speed = chaseMoveSpeed;
             agent.SetDestination(player.position);
         }
 
         if (idle && !resting) // Paths to a random point of interest
         {
             idle = false;
-            agent.speed = movementSpeed;
+            agent.speed = idleMoveSpeed;
             agent.SetDestination(pointsOfInterest[Random.Range(0, pointsOfInterest.Length)].position);
         }
         else if (agent.remainingDistance < 0.5f && !resting) // Has reached a destination, waits there for a while
@@ -56,8 +114,9 @@ public class EnemyBehaviour : MonoBehaviour
 
     IEnumerator Rest()
     {
-        yield return new WaitForSeconds(restingTime);
+        yield return new WaitForSeconds(waitTime);
         idle = true;
         resting = false;
     }
+    */
 }
