887147bda7e9267b1d47271ac822b29a518b66ad
Jere <jere.joensuu@tuni.fi>
2022-10-19 22:07:18 +0300

    Add exit door and collectables

---
+++ Assets/Code/Environment/Collectable.cs
@@ -0,0 +1,39 @@
+﻿using Code.Level;
+using UnityEngine;
+
+namespace Code.Environment
+{
+    public class Collectable : MonoBehaviour, IInteractable
+    {
+        private LevelManager _levelManager;
+        private bool _collected;
+
+        private void Start()
+        {
+            _levelManager = FindObjectOfType<LevelManager>();
+        }
+
+        private void Collect()
+        {
+            _levelManager.AddCollectable();
+            _collected = true;
+            // TODO: Play sound before destroying
+            Destroy(gameObject);
+        }
+
+        public bool IsCurrentlyInteractable()
+        {
+            return !_collected;
+        }
+
+        public string LookAtText()
+        {
+            return "Pick up";
+        }
+
+        public void Interact()
+        {
+            Collect();
+        }
+    }
+}
---
+++ Assets/Code/Level/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Code.Player;
+using UnityEngine;
+
+namespace Code.Level
+{
+    public class LevelManager : MonoBehaviour
+    {
+        private PlayerController _player;
+        public bool WinningConditionsMet { get; private set; }
+
+        [SerializeField] private int collectablesNeeded;
+
+        [SerializeField] private int collectablesInPossession;
+
+        public int CollectablesInPossession
+        {
+            get => collectablesInPossession;
+            set
+            {
+                collectablesInPossession = value;
+                WinningConditionsMet = collectablesInPossession >= collectablesNeeded;
+            }
+        }
+
+        private void Start()
+        {
+            _player = FindObjectOfType<PlayerController>();
+            collectablesInPossession = 0;
+        }
+
+        public void AddCollectable()
+        {
+            CollectablesInPossession++;
+        }
+    }
+}
