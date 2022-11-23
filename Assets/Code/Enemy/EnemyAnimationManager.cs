ab586e42df3220c2af9fc36591a524598fecfae0
Jere <jere.joensuu@tuni.fi>
2022-11-23 17:52:09 +0200

    Hotfix 2

--- Assets/Code/Enemy/EnemyAnimationManager.cs
+++ Assets/Code/Enemy/EnemyAnimationManager.cs
@@ -16,7 +16,6 @@ namespace Code.Enemy
         
         private void PlayAudio(EventReference eventReference)
         {
-            Debug.Log("Step");
             _audio = RuntimeManager.CreateInstance(eventReference);
             RuntimeManager.AttachInstanceToGameObject(_audio, GetComponent<Transform>(), GetComponent<Rigidbody>());
             _audio.start();
