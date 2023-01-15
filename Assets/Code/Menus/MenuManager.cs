b09bfa0816959d96f1f57eaf518205ab12e69be1
Matti Nurmi <matti.nurmi@tuni.fi>
2023-01-15 19:42:11 +0200

    enabled mouse in pause menu

--- Assets/Code/Menus/MenuManager.cs
+++ Assets/Code/Menus/MenuManager.cs
@@ -7,17 +7,37 @@ public class MenuManager : MonoBehaviour
 {
     public static bool gamePaused = false;
 
+    [SerializeField] private GameObject pauseMenu;
+
+    private void Awake()
+    {
+        gamePaused = false;
+        Time.timeScale = 1;
+    }
+
     public void PauseGame()
     {
         gamePaused = !gamePaused;
 
+        if (pauseMenu != null)
+        {
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+        }
+
         if (gamePaused)
         {
+            Cursor.lockState = CursorLockMode.None;
             Time.timeScale = 0;
         } else 
         {
+            Cursor.lockState = CursorLockMode.Locked;
             Time.timeScale = 1;
         }
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
     }
 
     public void StartGame()
@@ -28,5 +48,10 @@ public class MenuManager : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void Options()
+    {
+        // open options menu
     }
 }
