bd94d4cef5be37d3806f9cf2d7dd1a64f27cacc3
Matti Nurmi <matti.nurmi@tuni.fi>
2023-01-17 18:28:45 +0200

    can quit game in editor

--- Assets/Code/Menus/MenuManager.cs
+++ Assets/Code/Menus/MenuManager.cs
@@ -47,7 +47,13 @@ public class MenuManager : MonoBehaviour
 
     public void QuitGame()
     {
-        Application.Quit();
+        if (Application.isEditor)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        } else 
+        {
+            Application.Quit();
+        }
     }
 
     public void Options()
